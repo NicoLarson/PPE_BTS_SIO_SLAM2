fdbfe35fae4a3d0bbbc115bb34fa5025a43188c1
SkyDreamer223 <56580969+SkyDreamer223@users.noreply.github.com>
2021-02-12 15:56:30 +0100

    Update script_creation_de_tables.sql

--- script_creation_de_tables.sql
+++ script_creation_de_tables.sql
@@ -4,20 +4,23 @@ USE M2L;
 
 
 CREATE TABLE LIGUE (
-  id_ligue INT NOT NULL AUTO_INCREMENT,
+  id_ligue INT AUTO_INCREMENT,
   nom_ligue VARCHAR(255) NOT NULL,
   PRIMARY KEY (id_ligue));
 
 CREATE TABLE EMPLOYE (
-  id_emp INT NOT NULL AUTO_INCREMENT,
+  id_emp INT AUTO_INCREMENT,
   nom_emp VARCHAR(45) NOT NULL,
   prenom_emp VARCHAR(45) NOT NULL,
   mail_emp VARCHAR(255) NOT NULL,
   password_emp VARCHAR(255) NOT NULL,
-  admin_ligue BOOLEAN NULL,
-  super_admin BOOLEAN NULL,
+  date_arrive DATE NOT NULL,
+  date_depart DATE,
+  admin_ligue BOOLEAN NOT NULL DEFAULT 0,
+  super_admin BOOLEAN NOT NULL DEFAULT 0,
   id_ligue INT NULL,
   PRIMARY KEY (id_emp),
     FOREIGN KEY (id_ligue)
     REFERENCES LIGUE (id_ligue)
 );
+ 
