d8c344acdeb2ea9e970edbd04ccc80259e62e015
Nicolas Yang <61077274+NicoLarson@users.noreply.github.com>
2021-02-12 04:55:41 -0300

    Add DB script

---
+++ script_creation_de_tables.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE `M2L`;
+
+USE `M2L` ;
+
+
+CREATE TABLE IF NOT EXISTS `M2L`.`LIGUE` (
+  `id_ligue` INT NOT NULL AUTO_INCREMENT,
+  `nom_ligue` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id_ligue`));
+
+CREATE TABLE IF NOT EXISTS `M2L`.`EMPLOYE` (
+  `id_emp` INT NOT NULL AUTO_INCREMENT,
+  `nom_emp` VARCHAR(45) NOT NULL,
+  `prenom_emp` VARCHAR(45) NOT NULL,
+  `mail_emp` VARCHAR(255) NOT NULL,
+  `password_emp` VARCHAR(255) NOT NULL,
+  `admin_ligue` BOOLEAN NULL,
+  `super_admin` BOOLEAN NULL,
+  `id_ligue` INT NULL,
+  PRIMARY KEY (`id_emp`, `id_ligue`),
+    FOREIGN KEY (`id_ligue`)
+    REFERENCES `M2L`.`LIGUE` (`id_ligue`)
+);
