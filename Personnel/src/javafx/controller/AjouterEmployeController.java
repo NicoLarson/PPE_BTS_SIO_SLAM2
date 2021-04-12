cd2b54ca6a98d243bd3e9a307c9879677a2387c2
MathieuRabeyrin <mrabeyrin0@gmail.com>
2021-04-12 21:05:11 +0200

    Update AjouterEmploye view & controller

--- Personnel/src/javafx/controller/AjouterEmployeController.java
+++ Personnel/src/javafx/controller/AjouterEmployeController.java
@@ -5,13 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javafx.event.ActionEvent;
 
 import javafx.scene.layout.AnchorPane;
 import personnel.Ligue;
 import javafx.scene.control.PasswordField;
-
+import javafx.scene.control.Label;
 import javafx.scene.control.DatePicker;
 
 public class AjouterEmployeController {
@@ -30,18 +31,39 @@ public class AjouterEmployeController {
 	private PasswordField password;
 	@FXML
 	private DatePicker dateArrive;
+	@FXML
+	private Label warning;
 
 	
 	@FXML
-	public void quitter(ActionEvent event) throws IOException {
+	private void quitter(ActionEvent event) throws IOException {
 		AnchorPane anchor = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/view/Ligue.fxml"));
 		anchorPane.getChildren().setAll(anchor);
 	}
 	
+	private boolean checkField(String fieldName, String value)
+	{
+		if (value.length() == 0) {
+			warning.setText("Le champ " + fieldName + " est obligatoire");
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean checkRequiredFields()
+	{
+		return (checkField("nom", nom.getText()) && 
+				checkField("prénom", prenom.getText()) && 
+				checkField("mail", mail.getText()) && 
+				checkField("mot de passe", password.getText()));
+	}
+	
 	@FXML
-	public void valider(ActionEvent event) throws IOException {
-		ligue.addEmploye(nom.getText(), prenom.getText(), mail.getText(), password.getText(), dateArrive.getValue(), null);
-		AnchorPane anchor = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/view/Ligue.fxml"));
-		anchorPane.getChildren().setAll(anchor);
+	private void valider(ActionEvent event) throws IOException {
+		if (checkRequiredFields()) {
+			ligue.addEmploye(nom.getText(), prenom.getText(), mail.getText(), password.getText(), dateArrive.getValue(), null);
+			AnchorPane anchor = (AnchorPane) FXMLLoader.load(getClass().getResource("/javafx/view/Ligue.fxml"));
+			anchorPane.getChildren().setAll(anchor);
+		}
 	}
 }
